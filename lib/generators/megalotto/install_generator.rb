3dbda34f717ab0d1d47426e0632f42ffc362b92b
Peter McCready <petermccready2@gmail.com>
2018-12-09 18:39:20 +0000

    updated install generator to create file

--- lib/generators/megalotto/install_generator.rb
+++ lib/generators/megalotto/install_generator.rb
@@ -4,13 +4,13 @@ require 'rails/generators/base'
 module Megalotto
     module Generators
         class InstallGenerator < Rails::Generators::Base
-            puts 'install generator class accessed'
-            # source_root File.expand_path('templates', __dir__)
-            # desc 'This generator creates an initializer file at config/initializers/megalotto.rb'
+            puts 'Running install generator...'
+            source_root File.expand_path('templates', __dir__)
+            desc 'This generator creates an initializer file at config/initializers/megalotto.rb'
 
             def create_initializer_file
-                puts 'install generator create_initializer_file method accessed'
-                # template 'initializer.rb', 'config/initializers/megalotto.rb'
+                puts 'Creating initializer file at config/initializers/megalotto.rb'
+                template 'initializer.rb', 'config/initializers/megalotto.rb'
             end
         end
     end
